0a9664ed77564ae2233e902f550fce6db9f4603b
kakolats <akolatse1@gmail.com>
2025-10-29 10:04:16 +0000

    Menu UI update

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -6,15 +6,22 @@ export const defaultNavigation: FuseNavigationItem[] = [
         id   : 'create.event',
         title: 'Créer un evenement',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:plus',
         link : '/events/create-event'
     },
     {
         id   : 'list.event',
         title: 'Liste des évènements',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:list-bullet',
         link : '/events/event-list'
+    },
+    {
+        id   : 'list.event',
+        title: 'Deconnexion',
+        type : 'basic',
+        icon : 'logout',
+        link : '/sign-out'
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
